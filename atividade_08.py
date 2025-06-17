b60c2e4c2f60eaee3379519316b87be737be1045
HanielFreitas <hanielfreitas3@gmail.com>
2025-06-17 00:12:29 -0400

    feat removendo cadastro com campo email vazio

---
+++ atividade_08.py
@@ -0,0 +1,17 @@
+### Exercício 8. Filtragem de Dados Faltantes
+# Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
+
+
+usuarios = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
+
+usuario_validos = []
+
+for i in usuarios:
+    if i['email'] != '':
+        usuario_validos.append(i)
+
+print(usuario_validos)
